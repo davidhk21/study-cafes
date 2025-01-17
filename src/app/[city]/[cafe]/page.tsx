df90096f6b03b0f5d0243ba38df779298220203f
David Kim <David.Kim2@coxautoinc.com>
2025-01-17 17:35:00 -0500

    make selected cafe page mobile friendly

--- src/app/[city]/[cafe]/page.tsx
+++ src/app/[city]/[cafe]/page.tsx
@@ -28,19 +28,19 @@ const CityPage = async ({ params }: {
         </div>
       </div>
 
-      <div className="h-[100vh] flex items-center justify-center w-[80%] mx-auto">
+      <div className="md:h-[100vh] flex flex-col md:flex-row items-center justify-center md:w-[80%] mx-auto">
         <Image 
           src={selectedCafe.photos[1]}
           alt={selectedCafe.name}
-          className="flex-1 max-w-[600px] max-h-[800px] w-auto h-auto shadow-2xl"
+          className="flex-1 max-w-[90%] md:max-w-[600px] md:max-h-[800px] w-auto h-auto shadow-2xl m-12"
         />
-        <div className="flex-1 flex flex-col justify-center w-[50%] ml-12 max-w-[500px]">
+        <div className="flex-1 flex flex-col justify-center w-[90%] md:w-[50%] md:ml-12 max-w-[500px]">
           <p className="mb-6 font-sans leading-loose text-lg">Stumptown Coffee Roasters was one of the first cafes I visited upon moving to New York, and it has remained a favorite ever since. I often stop by after church or when I need a convenient workspace in the Midtown area. <br /><br /> The seating area is situated in a spacious lobby connecting the Ace Hotel and the cafe itself. With its diverse seating options and a lively atmosphere of people working or chatting over coffee, the space offers a pleasant balance of background noise without being overwhelmingly loud—perfect for those who appreciate a dynamic yet comfortable environment.</p>
         </div>
       </div>
 
-      <div className="flex flex-col md:flex-row justify-center items-center w-[80%] mx-auto">
-        <div className="flex-1 flex flex-col justify-center w-[50%] ml-12 max-w-[500px]">
+      <div className="flex flex-col md:flex-row justify-center items-center md:w-[80%] mx-auto">
+        <div className="flex-1 flex flex-col justify-center w-[90%] md:w-[50%] md:ml-12 max-w-[500px]">
         <h2 className="mb-6 font-libre tracking-widest text-content-title">RATINGS</h2>
           <p className="mb-6 font-sans leading-loose text-lg">Some really long description that has a bunch of words about the cafe and my personal opinion about thte cafe or what I like about the cafe that makes it worth adding to the list of cafes that I recommend</p>
           <p className="font-sans leading-loose text-lg">Some really long description that has a bunch of words about the cafe and my personal opinion about thte cafe or what I like about the cafe that makes it worth adding to the list of cafes that I recommend</p>
@@ -48,8 +48,8 @@ const CityPage = async ({ params }: {
         <Ratings ratings={selectedCafe.ratings} />
       </div>
       
-      <div className="flex flex-col md:flex-row justify-center items-center w-[80%] mx-auto my-20">
-        <div className="flex justify-center items-center">
+      <div className="flex flex-col md:flex-row justify-center items-center md:w-[80%] mx-auto md:my-20">
+        <div className="flex justify-center items-center mb-6 md:mb-0">
           <div className="bg-white flex items-center flex-col shadow-2xl">
             <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
               <h3 className="font-libre text-2xl">
@@ -71,34 +71,34 @@ const CityPage = async ({ params }: {
             </div>
           </div>
         </div>
-        <div className="flex flex-col justify-center w-[50%] max-w-[500px] ml-20">
+        <div className="flex flex-col justify-center w-[90%] md:w-[80%] md:ml-20 max-w-[500px]">
         <h2 className="mb-6 font-libre tracking-widest text-content-title">AMENITIES</h2>
           <p className="mb-6 font-sans leading-loose text-lg">Some really long description that has a bunch of words about the cafe and my personal opinion about thte cafe or what I like about the cafe that makes it worth adding to the list of cafes that I recommend</p>
           <p className="font-sans leading-loose text-lg">Some really long description that has a bunch of words about the cafe and my personal opinion about thte cafe or what I like about the cafe that makes it worth adding to the list of cafes that I recommend</p>
         </div>
       </div>
 
-      <div className="w-full h-[100vh] flex flex-col justify-center items-center">
+      <div className="w-full md:h-[100vh] flex flex-col justify-center items-center mt-6 md:mt-0">
         <Image 
           src={selectedCafe.photos[2]}
           alt={selectedCafe.name}
-          className="aspect-video max-w-[1000px] max-h-[700px] w-auto h-auto mb-12 shadow-2xl"
+          className="aspect-video max-w-[90%] md:max-w-[1000px] max-h-[700px] w-auto h-auto mb-12 shadow-2xl"
         />
-        <div className="flex flex-col items-center w-[50%] max-w-[700px] ml-12">
+        <div className="flex flex-col items-center w-[90%] md:w-[50%] max-w-[700px] md:ml-12">
           <h2 className="mb-6 font-libre tracking-widest text-content-title">AESTHETIC</h2>
           <p className="font-sans leading-loose text-lg">Some really long description that has a bunch of words about the cafe and my personal opinion about thte cafe or what I like about the cafe that makes it worth adding to the list of cafes that I recommend</p>
         </div>
       </div>
 
-      <div className="flex flex-col md:flex-row justify-center items-center w-[80%] h-[60vh] mx-auto py-12">
+      <div className="flex flex-col md:flex-row justify-center items-center w-[90%] md:w-[80%] mx-auto py-12">
         <iframe
-          className="min-w-[250px] min-h-[300px] max-w-[700px] w-full h-full"
+          className="min-w-[250px] min-h-[300px] max-w-[700px] w-full h-full mb-6 md:mb-0"
           loading="lazy"
           allowFullScreen
           referrerPolicy="no-referrer-when-downgrade"
           src={`https://www.google.com/maps/embed/v1/place?key=${process.env.GOOGLE_MAPS_API_KEY}&q=${selectedCafe.mapsQuery}`}>
         </iframe>
-        <div className="flex flex-col justify-center w-[50%] max-w-[500px] ml-12">
+        <div className="flex flex-col justify-center md:w-[50%] max-w-[500px] md:ml-12">
         <h2 className="mb-6 font-libre tracking-widest text-content-title">LOCATION</h2>
           <p className="mb-6 font-sans leading-loose text-lg">Some really long description that has a bunch of words about the cafe and my personal opinion about thte cafe or what I like about the cafe that makes it worth adding to the list of cafes that I recommend</p>
           <p className="font-sans leading-loose text-lg">Some really long description that has a bunch of words about the cafe and my personal opinion about thte cafe or what I like about the cafe that makes it worth adding to the list of cafes that I recommend</p>
